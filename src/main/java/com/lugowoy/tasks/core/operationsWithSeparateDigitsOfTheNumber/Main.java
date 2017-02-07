3eb50a857ac9efd8cd57c31f2dba6568a18d317e
Lugowoy Constantin <kostya.lugowoy@gmail.com>
2017-02-07 20:43:58 +0200

    07.02.2017 1) Finish the task code from the package "com.lugowoy.tasks.core.operationsWithSeparateDigitsOfTheNumber".

--- src/main/java/com/lugowoy/tasks/core/operationsWithSeparateDigitsOfTheNumber/Main.java
+++ src/main/java/com/lugowoy/tasks/core/operationsWithSeparateDigitsOfTheNumber/Main.java
@@ -1,5 +1,9 @@
 package com.lugowoy.tasks.core.operationsWithSeparateDigitsOfTheNumber;
 
+import com.lugowoy.util.reading.ReadingData;
+import com.lugowoy.util.reading.ReadingUserInputData;
+import com.lugowoy.util.reading.StoppingEnterValueException;
+
 /**
  * Created by Konstantin on 05-Feb-17.
  *
@@ -7,10 +11,36 @@ package com.lugowoy.tasks.core.operationsWithSeparateDigitsOfTheNumber;
  */
 public class Main {
 
-    public static void main(String[] args) {
+    private static final ReadingData<Integer> INTEGER_READING_DATA = new ReadingData<>(new ReadingUserInputData()::readInt);
 
+    public static void main(String[] args) throws StoppingEnterValueException {
 
-        System.out.println(ParserIntegerNumber.getSingleDigitOfTheNumber(45678735, 2));
+        Number number;
+
+        System.out.println("Enter the four-digit number : ");
+        int enteredNumber;
+        do {
+            enteredNumber = INTEGER_READING_DATA.read();
+            String enteredNumberForVerification = Integer.toString(enteredNumber);
+            if (enteredNumberForVerification.length() == 4) {
+                number = new Number(enteredNumber);
+                break;
+            } else {
+                System.out.println("Entered incorrect number. Re-enter : ");
+            }
+        } while (true);
+
+        int firstDigitOfNumber = ParserIntegerNumber.getSingleDigitOfTheNumber(number.getNumber(), 1);
+        int lastDigitOfNumber = ParserIntegerNumber.getSingleDigitOfTheNumber(number.getNumber(), 4);
+
+        System.out.printf("The sum of the first and last digit numbers contained in the entered number equal : %d", firstDigitOfNumber + lastDigitOfNumber);
+
+        System.out.println();
+
+        int secondDigitOfNumber = ParserIntegerNumber.getSingleDigitOfTheNumber(number.getNumber(), 2);
+        int thirdDigitOfNumber = ParserIntegerNumber.getSingleDigitOfTheNumber(number.getNumber(), 3);
+
+        System.out.printf("The difference between the second and third digit numbers contained in the entered number equal : %d", secondDigitOfNumber - thirdDigitOfNumber);
 
     }
 
