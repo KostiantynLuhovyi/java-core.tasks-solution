1d868062c649833d556eb0d3926b46522b7391f0
Lugowoy Constantin <kostya.lugowoy@gmail.com>
2017-02-12 14:33:50 +0200

    12.02.2017 Solved task in the package com.lugowoy.tasks.arrays.findTheSumOfAnArrayOfMultipleElementsToThisPresent; Description task in the file .tasks.arrays.findTheSumOfAnArrayOfMultipleElementsToThisPresent.txt.

---
+++ src/main/java/com/lugowoy/tasks/arrays/findTheSumOfAnArrayOfMultipleElementsToThisPresent/FindingSumElements.java
@@ -0,0 +1,9 @@
+package com.lugowoy.tasks.arrays.findTheSumOfAnArrayOfMultipleElementsToThisPresent;
+
+/**Created by Konstantin Lugowoy on 12-Feb-17.*/
+
+@FunctionalInterface
+public interface FindingSumElements {
+
+    int getSumElements(Numbers numbers);
+}
---
+++ src/main/java/com/lugowoy/tasks/arrays/findTheSumOfAnArrayOfMultipleElementsToThisPresent/Main.java
@@ -0,0 +1,46 @@
+package com.lugowoy.tasks.arrays.findTheSumOfAnArrayOfMultipleElementsToThisPresent;
+
+import com.lugowoy.util.fillable.FillableArray;
+import com.lugowoy.util.reading.ReadingRandomData;
+import com.lugowoy.util.reading.ReadingUserInputData;
+import com.lugowoy.util.reading.StoppingEnterValueException;
+
+/**Created by Konstantin Lugowoy on 12-Feb-17.*/
+
+public class Main {
+
+    private static final FindingSumElements FINDING_SUM_ELEMENTS = numbers -> {
+        int sumElements = 0;
+        for (int i = 0; i < numbers.getSourceArray().length; i++) {
+            if (numbers.getSourceArray()[i] != null) {
+                if (numbers.getSourceArray()[i] % numbers.getPresentK() == 0) {
+                    sumElements += numbers.getSourceArray()[i];
+                }
+            }
+        }
+        return sumElements;
+    };
+
+    private static final FillableArray<Integer> INTEGER_FILLABLE_ARRAY = new FillableArray<>(new ReadingUserInputData()::readInt);
+
+    public static void main(String[] args) throws StoppingEnterValueException {
+
+        Numbers numbers = new Numbers();
+        numbers.setSourceArray(INTEGER_FILLABLE_ARRAY.fillArray(new Integer[5]));
+
+        System.out.println("Result fill arrays randomly integers : ");
+        for (int i = 0; i < numbers.getSourceArray().length; i++) {
+            if (numbers.getSourceArray()[i] != null) {
+                System.out.print(numbers.getSourceArray()[i] + " ");
+            }
+        }
+        System.out.println();
+
+        System.out.println("Enter K number : ");
+        numbers.setPresentK(new ReadingUserInputData().readInt());
+
+        numbers.setResultOfSumNumbersOfMultipleElement(FINDING_SUM_ELEMENTS.getSumElements(numbers));
+
+        System.out.println("Result : " + numbers.getResultOfSumNumbersOfMultipleElement());
+    }
+}
