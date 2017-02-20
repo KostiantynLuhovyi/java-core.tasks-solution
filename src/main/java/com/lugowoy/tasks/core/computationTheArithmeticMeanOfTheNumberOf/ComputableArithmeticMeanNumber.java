811c459052108d7e76be3ed483e8b00e82922d80
Lugowoy Constantin <kostya.lugowoy@gmail.com>
2017-02-20 21:26:42 +0200

    20.02.2017 Edit code in the package com.lugowoy.tasks.core.computationTheArithmeticMeanNumberOf;

---
+++ src/main/java/com/lugowoy/tasks/core/computationTheArithmeticMeanOfTheNumberOf/ComputableArithmeticMeanNumber.java
@@ -0,0 +1,9 @@
+package com.lugowoy.tasks.core.computationTheArithmeticMeanOfTheNumberOf;
+
+/**Created by Konstantin Lugowoy on 03-Dec-16.*/
+
+public interface ComputableArithmeticMeanNumber {
+
+    double computationArithmeticMeanNumber(Double[] doublesArray);
+
+}
---
+++ src/main/java/com/lugowoy/tasks/core/computationTheArithmeticMeanOfTheNumberOf/ComputationArithmeticMeanNumbers.java
@@ -0,0 +1,21 @@
+package com.lugowoy.tasks.core.computationTheArithmeticMeanOfTheNumberOf;
+
+/**Created by Konstantin Lugowoy on 03-Dec-16.*/
+
+public class ComputationArithmeticMeanNumbers implements ComputableArithmeticMeanNumber {
+
+    @Override
+    public double computationArithmeticMeanNumber(Double[] doublesArray) {
+        Double resultArithmeticMeanNumber = 0d;
+
+        for (Double number : doublesArray) {
+            if (number != null) {
+                resultArithmeticMeanNumber += number;
+            }
+        }
+        resultArithmeticMeanNumber /= (doublesArray.length-1);
+
+        return resultArithmeticMeanNumber;
+    }
+
+}
