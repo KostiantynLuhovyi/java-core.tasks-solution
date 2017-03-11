f57f3447419406a3222df7c423fef2413017aa62
Lugowoy Constantin <kostya.lugowoy@gmail.com>
2017-03-11 11:42:53 +0200

    11.03.2017 Solved task in the package com.lugowoy.tasks.arrays.replaceWithZerosElementsThatAreModuloGreaterThanTheMaximumElement; Description task in the file tasks.arrays.replaceWithZerosElementsThatAreModuloGreaterThanTheMaximumElement .

---
+++ src/main/java/com/lugowoy/tasks/arrays/replaceWithZerosElementsThatAreModuloGreaterThanTheMaximumElement/Main.java
@@ -0,0 +1,42 @@
+package com.lugowoy.tasks.arrays.replaceWithZerosElementsThatAreModuloGreaterThanTheMaximumElement;
+
+import com.lugowoy.util.fillable.Fillable;
+import com.lugowoy.util.fillable.FillingArrayOfRandomNumber;
+import com.lugowoy.util.reading.ReadingRandomData;
+
+import java.util.Arrays;
+
+/** Created by Konstantin Lugowoy on 11.03.2017. */
+
+public class Main {
+
+    private static final Fillable<Integer> FILLABLE = new FillingArrayOfRandomNumber<>(new ReadingRandomData()::readInt);
+
+    public static void main(String[] args) {
+
+        Integer[] integers = FILLABLE.fillArray(new Integer[30]);
+
+        Numbers numbers = new Numbers(integers);
+
+        System.out.println("Original sequence : ");
+        Arrays.stream(numbers.getIntNumbers()).forEachOrdered(integer -> System.out.print(integer + " "));
+        System.out.println();
+
+        int maximumNumberElement = 0;
+
+        if (Arrays.stream(numbers.getIntNumbers()).max(Integer::compareTo).isPresent()) {
+            maximumNumberElement = Arrays.stream(numbers.getIntNumbers()).max(Integer::compareTo).get();
+        }
+
+        System.out.println("Result sequence : ");
+        int finalMaximumNumberElement = maximumNumberElement;
+        Arrays.stream(numbers.getIntNumbers()).forEachOrdered(integer -> {
+            if (finalMaximumNumberElement != 0) {
+                if (Math.abs(integer) > finalMaximumNumberElement) {
+                    integer = 0;
+                }
+            }
+            System.out.print(integer + " ");
+        });
+    }
+}
---
+++ src/main/java/com/lugowoy/tasks/arrays/replaceWithZerosElementsThatAreModuloGreaterThanTheMaximumElement/Numbers.java
@@ -0,0 +1,19 @@
+package com.lugowoy.tasks.arrays.replaceWithZerosElementsThatAreModuloGreaterThanTheMaximumElement;
+
+/** Created by Konstantin Lugowoy on 11.03.2017. */
+
+public class Numbers {
+
+    private Integer[] intNumbers;
+
+    public Numbers() {
+    }
+
+    public Numbers(Integer[] intNumbers) {
+        this.intNumbers = intNumbers;
+    }
+
+    public Integer[] getIntNumbers() {
+        return intNumbers;
+    }
+}
