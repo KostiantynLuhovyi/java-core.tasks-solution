1ded363f2382fd0737b120a16a92fe6c6c16ba48
Lugowoy Constantin <kostya.lugowoy@gmail.com>
2017-05-11 15:37:00 +0300

    Solved task in the package com.lugowoy.tasks.arrays.locateSnailOnTreeAfterThirtyDays; Description task in the file tasks.arrays.locateSnailOnTreeAfterThirtyDays.txt .

---
+++ src/main/java/com/lugowoy/tasks/arrays/locateSnailOnTreeAfterThirtyDays/Main.java
@@ -0,0 +1,55 @@
+package com.lugowoy.tasks.arrays.locateSnailOnTreeAfterThirtyDays;
+
+import com.lugowoy.tasks.arrays.locateSnailOnTreeAfterThirtyDays.entity.Snail;
+import com.lugowoy.tasks.arrays.locateSnailOnTreeAfterThirtyDays.entity.Tree;
+import com.lugowoy.tasks.arrays.locateSnailOnTreeAfterThirtyDays.entity.Weather;
+import com.lugowoy.tasks.arrays.locateSnailOnTreeAfterThirtyDays.entity.WeatherArray;
+import com.lugowoy.tasks.oop.determiningProfitableExchangeOfCurrencyInBank.determine.*;
+import com.lugowoy.util.reading.ReadingData;
+import com.lugowoy.util.reading.ReadingUserInputData;
+
+import java.util.Arrays;
+import java.util.Random;
+
+/** Created by Konstantin Lugowoy on 11.05.2017. */
+
+public class Main {
+
+    private static ReadingData<Integer> readingData = new ReadingData<>(new ReadingUserInputData()::readInt);
+
+    public static void main(String[] args) {
+
+        System.out.println("Enter the height of the tree : ");
+        Tree tree = new Tree(readingData.read());
+
+        System.out.println("Enter the starting location of a snail on the tree : ");
+        Snail snail = new Snail(readingData.read());
+
+        tree.setSnail(snail);
+
+        System.out.println("Enter the number of days of observation : ");
+        WeatherArray weatherArray = new WeatherArray(readingData.read());
+
+        weatherArray.setWeathers(Arrays.stream(new Weather[weatherArray.getNumberOfDaysOfWeatherObservation()])
+                                       .map(weather -> weather = new Weather())
+                                       .toArray(Weather[]::new));
+
+        fillWeatherCloudinessInTheWeatherArray(weatherArray);
+
+        Determinable<Tree, WeatherArray> determinable = Determinable::determine;
+        Determinable.determine(tree, weatherArray);
+
+    }
+
+    private static void fillWeatherCloudinessInTheWeatherArray(WeatherArray weatherArray) {
+        Random random = new Random();
+
+        Arrays.stream(weatherArray.getWeathers()).forEachOrdered(weather -> {
+            int tmp = random.nextInt();
+            if (tmp > 0) {
+                weather.setCloudiness(true);
+            }
+        });
+
+    }
+}
---
+++ src/main/java/com/lugowoy/tasks/arrays/locateSnailOnTreeAfterThirtyDays/entity/Weather.java
@@ -0,0 +1,19 @@
+package com.lugowoy.tasks.arrays.locateSnailOnTreeAfterThirtyDays.entity;
+
+/** Created by Konstantin Lugowoy on 11.05.2017. */
+
+public class Weather {
+
+    private boolean cloudiness;
+
+    public Weather() {
+    }
+
+    public boolean isCloudiness() {
+        return cloudiness;
+    }
+
+    public void setCloudiness(boolean cloudiness) {
+        this.cloudiness = cloudiness;
+    }
+}
