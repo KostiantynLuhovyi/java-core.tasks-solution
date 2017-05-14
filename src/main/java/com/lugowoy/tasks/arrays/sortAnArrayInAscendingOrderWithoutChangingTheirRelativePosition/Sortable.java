e981cd76433a5c4ce1247ad7a59b98da2e6b5816
Lugowoy Constantin <kostya.lugowoy@gmail.com>
2017-05-14 16:25:28 +0300

    Solved task in the package com.lugowoy.tasks.arrays.sortAnArrayInAscendingOrderWithoutChangingTheirRelativePosition; Description task in the file tasks.arrays.sortAnArrayInAscendingOrderWithoutChangingTheirRelativePosition.txt  .

---
+++ src/main/java/com/lugowoy/tasks/arrays/sortAnArrayInAscendingOrderWithoutChangingTheirRelativePosition/Sortable.java
@@ -0,0 +1,24 @@
+package com.lugowoy.tasks.arrays.sortAnArrayInAscendingOrderWithoutChangingTheirRelativePosition;
+
+/** Created by Konstantin Lugowoy on 14.05.2017. */
+
+@FunctionalInterface
+public interface Sortable<T> {
+
+    void sort(Array<T> array);
+
+    static void sortAnArrayInAscendingOrderWithoutChangingTheirRelativePosition(Array<Integer> array) {
+        int tmp;
+
+        for (int i = 0; i < array.getArray().length; i++) {
+            for (int j = 0; j < array.getArray().length - i - 1; j++) {
+                if (((array.getArray()[j] == 0) && (array.getArray()[j + 1] < 0))
+                        || ((array.getArray()[j] > 0) && (array.getArray()[j + 1] <= 0 ))) {
+                    tmp = array.getArray()[j];
+                    array.getArray()[j] = array.getArray()[j + 1];
+                    array.getArray()[j + 1] = tmp;
+                }
+            }
+        }
+    }
+}
