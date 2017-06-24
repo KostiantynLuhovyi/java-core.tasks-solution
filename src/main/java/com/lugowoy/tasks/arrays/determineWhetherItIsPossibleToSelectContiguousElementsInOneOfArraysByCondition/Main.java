c5b36f54ca4b5e4aed74e8b094050820a2b3c8f8
Lugowoy Constantin <kostya.lugowoy@gmail.com>
2017-06-24 20:50:09 +0300

    ► Solved task in the package com.lugowoy.tasks.arrays.determineWhetherItIsPossibleToSelectContiguousElementsInOneOfArraysByCondition;      Description task in the file tasks.arrays.determineWhetherItIsPossibleToSelectContiguousElementsInOneOfArrays.txt  .

---
+++ src/main/java/com/lugowoy/tasks/arrays/determineWhetherItIsPossibleToSelectContiguousElementsInOneOfArraysByCondition/Main.java
@@ -0,0 +1,43 @@
+package com.lugowoy.tasks.arrays.determineWhetherItIsPossibleToSelectContiguousElementsInOneOfArraysByCondition;
+
+import com.lugowoy.util.factory.creating.arrays.CreatorArray;
+import com.lugowoy.util.factory.creating.arrays.CreatorArrayOfIntegerPrimitives;
+import com.lugowoy.util.filling.arrays.FillingArray;
+import com.lugowoy.util.filling.arrays.FillingArrayOfRandomNumber;
+import com.lugowoy.util.filling.arrays.FillingArrayOfUserInput;
+import com.lugowoy.util.models.arrays.Array;
+import com.lugowoy.util.reading.ReadingRandomData;
+import com.lugowoy.util.reading.ReadingUserInputSizeOfTheArray;
+
+import java.util.Arrays;
+import java.util.List;
+
+/** Created by Konstantin Lugowoy on 22.06.2017. */
+
+public class Main {
+
+    private static FillingArray<Integer> fillingArray = new FillingArrayOfRandomNumber<>(new ReadingRandomData()::readInt);
+    private static CreatorArray<Integer> creatorArray = new CreatorArrayOfIntegerPrimitives();
+
+    public static void main(String[] args) {
+
+        int sizeToFirstArray = ReadingUserInputSizeOfTheArray.enterUserInputForSizeOfTheArray("Enter a value for the size of the first array : ");
+
+        int sizeToSecondArray = ReadingUserInputSizeOfTheArray.enterUserInputForSizeOfTheArray("Enter a value for the size of second array : ");
+
+        Array<Integer> firstArray = creatorArray.create(fillingArray.fillArray(new int[sizeToFirstArray], 100));
+        Array<Integer> secondArray = creatorArray.create(fillingArray.fillArray(new int[sizeToSecondArray], 100));
+
+        Determine<Array<Integer>> determine = Determine::determineWhetherItIsPossibleToSelectContiguousElements;
+        List<Array<Integer>> arrayList = determine.determine(firstArray, secondArray);
+
+        if (!arrayList.isEmpty()) {
+            System.out.println("Result : ");
+            arrayList.forEach(array -> System.out.println(Arrays.toString(array.getArrayOfIntegerPrimitives()) + " "));
+        } else {
+            System.out.println("Result array is empty.");
+        }
+
+    }
+
+}
