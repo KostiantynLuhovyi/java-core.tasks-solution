5a9d4f5b2babe508269a490b3a9af955a506775c
Lugowoy Constantin <kostya.lugowoy@gmail.com>
2017-08-18 23:42:13 +0300

    Solved task in the package com.lugowoy.tasks.core.determineThatNumberIsHappy;  Description task in the file determineThatNumberIsHappy.txt .

---
+++ src/main/java/com/lugowoy/tasks/core/determineThatNumberIsHappy/DeterminableThatNumberIsHappy.java
@@ -0,0 +1,9 @@
+package com.lugowoy.tasks.core.determineThatNumberIsHappy;
+
+/** Created by Konstantin Lugowoy on 17.08.2017. */
+
+public interface DeterminableThatNumberIsHappy {
+
+    boolean determineThatNumberIsHappy(int number);
+
+}
