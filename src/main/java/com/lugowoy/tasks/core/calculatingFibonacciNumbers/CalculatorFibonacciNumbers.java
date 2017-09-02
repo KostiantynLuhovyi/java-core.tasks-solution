dae71cdf39943f957be3af3fa4aca6e2c6f7a21f
Lugowoy Constantin <kostya.lugowoy@gmail.com>
2017-09-02 23:06:42 +0300

    Solved task in the package com.lugowoy.tasks.core.calculatingFibonacciNumbers;  Description task in the file calculatingFibonacciNumbers.txt .

---
+++ src/main/java/com/lugowoy/tasks/core/calculatingFibonacciNumbers/CalculatorFibonacciNumbers.java
@@ -0,0 +1,11 @@
+package com.lugowoy.tasks.core.calculatingFibonacciNumbers;
+
+/** Created by Konstantin Lugowoy on 02.09.2017. */
+
+public abstract class CalculatorFibonacciNumbers {
+
+    public abstract int calculatingOfPositiveFibonacciNumbers(int number);
+
+    public abstract int calculatingOfNegativeFibonacciNumbers(int number);
+
+}
