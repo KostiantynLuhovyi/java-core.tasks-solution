30f31b65ad56621360561c8f59a9807138da2b26
Lugowoy Constantin <kostya.lugowoy@gmail.com>
2017-09-03 12:10:27 +0300

    Solved task in the package com.lugowoy.tasks.core.determineNumberIsPolindrome;  Description task in the file determineNumberIsPolindrome.txt .

---
+++ src/main/java/com/lugowoy/tasks/core/determineNumberIsPolindrome/Determinator.java
@@ -0,0 +1,24 @@
+package com.lugowoy.tasks.core.determineNumberIsPolindrome;
+
+/** Created by Konstantin Lugowoy on 03.09.2017. */
+
+@FunctionalInterface
+public interface Determinator<T> {
+
+    boolean determine(T t);
+
+    static boolean determineNumberIsPolindrome(int number) {
+        String numberValue = Integer.toString(number);
+
+        int duplicateNumber = number;
+        int reverseNumber = 0;
+
+        for (int i = 0; i < numberValue.length(); i++) {
+            reverseNumber = reverseNumber * 10 + (duplicateNumber % 10);
+            duplicateNumber /= 10;
+        }
+
+        return number == reverseNumber;
+    }
+
+}
