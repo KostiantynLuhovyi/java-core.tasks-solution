01ddc5dca940a7697cdc63b38d394677c3875bb7
Lugowoy Constantin <kostya.lugowoy@gmail.com>
2017-10-02 00:14:37 +0300

    Solved task in the package com.lugowoy.tasks.core.checkCorrectnessOfEnteredTimeData;   Description task in the file checkCorrectnessOfEnteredTimeData.txt  .

---
+++ src/main/java/com/lugowoy/tasks/core/checkCorrectnessOfEnteredTimeData/Main.java
@@ -0,0 +1,40 @@
+package com.lugowoy.tasks.core.checkCorrectnessOfEnteredTimeData;
+
+import com.lugowoy.helper.reading.Reader;
+import com.lugowoy.helper.reading.ReadingDataUserInputInConsole;
+
+/** Created by Konstantin Lugowoy on 01.10.2017. */
+
+public class Main {
+
+    private static final Reader READER = new Reader(new ReadingDataUserInputInConsole());
+
+    public static void main(String[] args) {
+
+        System.out.println("Enter hours : ");
+        int hours = READER.readInt();
+        System.out.println("Enter minutes : ");
+        int minutes = READER.readInt();
+        System.out.println("Enter seconds : ");
+        int seconds = READER.readInt();
+
+        Checker checker = CheckerHours::checkCorrectHours;
+        if (checker.check(hours)) {
+            checker = CheckerMinutes::checkCorrectMinutes;
+            if (checker.check(minutes)) {
+                checker = CheckerSeconds::checkCorrectSeconds;
+                if (checker.check(seconds)) {
+                    System.out.printf("Time : %d hours, %d minutes, %d seconds .", hours, minutes, seconds);
+                } else {
+                    System.out.println("The value of seconds is invalid.");
+                }
+            } else {
+                System.out.println("The value of minutes is invalid.");
+            }
+        } else {
+            System.out.println("The value of hours is invalid.");
+        }
+
+    }
+
+}
