4c9f28ea0c5949b1daed8afaef222490b0b308f4
Lugowoy Constantin <kostya.lugowoy@gmail.com>
2017-10-06 14:57:36 +0300

    06.10.2017  14:57  • Solved task in the package com.lugowoy.tasks.core.outputAllNumbersToWhichEnteredNumberIsDividedWithoutRemainder;      Description task in the file outputAllNumbersToWhichEnteredNumberIsDividedWithoutRemainder.txt  .

---
+++ src/main/java/com/lugowoy/tasks/core/outputAllNumbersToWhichEnteredNumberIsDividedWithoutRemainder/Main.java
@@ -0,0 +1,26 @@
+package com.lugowoy.tasks.core.outputAllNumbersToWhichEnteredNumberIsDividedWithoutRemainder;
+
+import com.lugowoy.helper.reading.Reader;
+import com.lugowoy.helper.reading.ReadingDataUserInputInConsole;
+
+/** Created by Konstantin Lugowoy on 06.10.2017. */
+
+public class Main {
+
+    private static final Reader READER = new Reader(new ReadingDataUserInputInConsole());
+
+    public static void main(String[] args) {
+
+        System.out.println("Enter number : ");
+        int number = READER.readInt();
+
+        System.out.println("The numbers to divide the entered number without a remainder : ");
+        for (int i = 1; i <= number; i++) {
+            if (number % i == 0) {
+                System.out.print(i + " ");
+            }
+        }
+
+    }
+
+}
