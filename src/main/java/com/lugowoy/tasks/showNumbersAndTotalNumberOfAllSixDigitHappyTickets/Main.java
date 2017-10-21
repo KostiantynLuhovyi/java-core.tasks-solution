d86eafc8f10845ab75bbd002d1e6143ec4361750
Lugowoy Constantin <kostya.lugowoy@gmail.com>
2017-10-21 21:52:00 +0300

    21.10.2017   21:51  • Solved task in the package showNumbersAndTotalNumberOfAllSixDigitHappyTickets;    Description task in the file showNumbersAndTotalNumberOfAllSixDigitHappyTickets.txt  .

---
+++ src/main/java/com/lugowoy/tasks/showNumbersAndTotalNumberOfAllSixDigitHappyTickets/Main.java
@@ -0,0 +1,14 @@
+package com.lugowoy.tasks.showNumbersAndTotalNumberOfAllSixDigitHappyTickets;
+
+/** Created by Konstantin Lugowoy on 21.10.2017. */
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Display display = Display::displayAllHappyTickets;
+        display.display();
+
+    }
+
+}
