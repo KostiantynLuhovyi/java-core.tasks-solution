792e9073bd06e92e10b8dc371f48192cab6fb73a
Lugowoy Constantin <kostya.lugowoy@gmail.com>
2017-10-25 19:15:29 +0300

    25.10.2017   19:15  • Solved task in the package displayPythagoreanMultiplicationTable;    Description task in the file displayPythagoreanMultiplicationTable.txt  .

---
+++ src/main/java/com/lugowoy/tasks/displayPythagoreanMultiplicationTable/Main.java
@@ -0,0 +1,21 @@
+package com.lugowoy.tasks.displayPythagoreanMultiplicationTable;
+
+/** Created by Konstantin Lugowoy on 25.10.2017. */
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println("Pythagorean multiplication table : ");
+
+
+        for (int i = 1; i <= 9; i++) {
+            for (int j = 1; j <= 9; j++) {
+                System.out.printf("%4d", i * j);
+            }
+            System.out.println();
+        }
+
+    }
+
+}
