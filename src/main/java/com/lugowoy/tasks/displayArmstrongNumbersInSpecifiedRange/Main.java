1ec613d1c93e2b2ea1ef74b2cdcd20de279caa41
Lugowoy Constantin <kostya.lugowoy@gmail.com>
2017-11-02 22:18:19 +0200

    02.11.2017   22:18  • Solved task in the package com.lugowoy.tasks.displayArmstrongNumbersInSpecifiedRange;    Description task in the file displayArmstrongNumbersInSpecifiedRange.txt  .

---
+++ src/main/java/com/lugowoy/tasks/displayArmstrongNumbersInSpecifiedRange/Main.java
@@ -0,0 +1,43 @@
+package com.lugowoy.tasks.displayArmstrongNumbersInSpecifiedRange;
+
+/** Created by Konstantin Lugowoy on 02.11.2017. */
+
+public class Main {
+
+    private static final int BOUND = 10_000_000;
+
+    public static void main(String[] args) {
+
+        for (int i = 0; i <= BOUND; i++) {
+            if (isAmstrongNumber(i)) {
+                System.out.print(i + ", ");
+            }
+        }
+    }
+
+    private static int numberOfDigit(int number) {
+        int numberOfDigit = 0;
+        if (number > 0)
+            while (number != 0)
+            {
+                number = number / 10;
+                numberOfDigit++;
+            }
+        return numberOfDigit;
+    }
+
+    private static boolean isAmstrongNumber(int number) {
+        int digit;
+        int result = 0;
+        int tmpNumber = number;
+        int count = numberOfDigit(number);
+
+        while (number != 0) {
+            digit = number % 10;
+            result = result+((int) Math.pow(digit, count));
+            number = number / 10;
+        }
+        return tmpNumber == result;
+    }
+
+}
