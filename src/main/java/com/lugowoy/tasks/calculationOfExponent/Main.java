1a60ff4717c1b4e5c2020ec94a9910d09f408593
Lugowoy Constantin <kostya.lugowoy@gmail.com>
2017-11-10 21:59:35 +0200

    10.11.2017   21:59  • Solved task in the package com.lugowoy.tasks.calculationOfExponent;    Description task in the file calculationOfExponent.txt .

---
+++ src/main/java/com/lugowoy/tasks/calculationOfExponent/Main.java
@@ -0,0 +1,22 @@
+package com.lugowoy.tasks.calculationOfExponent;
+
+/** Created by Konstantin Lugowoy on 10.11.2017. */
+
+public class Main {
+
+    private static final int BOUND = 1_000_000;
+
+    public static void main(String[] args) {
+
+        double x = 1, q = 1, s = 0;
+
+        for(int i = 0; i <= BOUND; i++) {
+            s += q;
+            q *= x / (i + 1);
+        }
+
+        System.out.println("Exponent \"x\" : " + s);
+
+    }
+
+}
